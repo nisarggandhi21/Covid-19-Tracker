ff2dcbe90158e7788f6b97be3ac065de3af6d4df
Nisarg Gandhi <nisarggandhi21@gmail.com>
2020-05-22 14:04:33 +0530

    v1

---
+++ precache-manifest.f75ba6ac926a76bd80fb3efbffcfc699.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0d763988764e1a4024a25d68a671c17",
+    "url": "/project/Project/covid_19/index.html"
+  },
+  {
+    "revision": "941fcba59c0b497b4d9f",
+    "url": "/project/Project/covid_19/static/css/main.007ab4ea.chunk.css"
+  },
+  {
+    "revision": "615dbcde5b51d4f237d5",
+    "url": "/project/Project/covid_19/static/js/2.5ff53c30.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/project/Project/covid_19/static/js/2.5ff53c30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "941fcba59c0b497b4d9f",
+    "url": "/project/Project/covid_19/static/js/main.51989237.chunk.js"
+  },
+  {
+    "revision": "2ebdee43b4462ca0badd",
+    "url": "/project/Project/covid_19/static/js/runtime-main.cb7be2f4.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/project/Project/covid_19/static/media/image.d7265326.png"
+  }
+]);
